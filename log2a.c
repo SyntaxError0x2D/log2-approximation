1bcd29e271b6ba4e7ec1a6445f3ca0aa50e2588c
sandem45 <116912414+SyntaxError0x2D@users.noreply.github.com>
2023-04-17 22:03:43 +0300

    changed expmnt to expnt as there is no m in exponent.

--- log2a.c
+++ log2a.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 const int mask = (1 << 23) -1;
-const int expmnt = -1 - mask;
+const int expnt = -1 - mask;
 
 int binLeng( unsigned int bin ) {
     for (int i = sizeof(int) * 8-1; i > 0; i--) {
@@ -18,9 +18,8 @@ float log2a( int n ) {
     
     int l = binLeng(n);
     float r = (n & ( 1 << ( l - 1 ) ) - 1 );
-    int tmp = (( (* (int *) &r) & expmnt) -((l-1)  << 23))  | (( (* (int *) &r) & mask));
+    int tmp = (( (* (int *) &r) & expnt) -((l-1)  << 23))  | (( (* (int *) &r) & mask));
    
 
     return( ((float) l - 1) + * (float *) &tmp );
-
 }
