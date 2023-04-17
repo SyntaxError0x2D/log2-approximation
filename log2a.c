aa1703500390f559bacc3498ada6b8632882e388
sandem45 <116912414+SyntaxError0x2D@users.noreply.github.com>
2023-04-17 19:19:30 +0300

    Wrote it in c.
    0% c experience, thought it'd be fun, it was.
    Feel free to improve!

---
+++ log2a.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+const int mask = (1 << 23) -1;
+const int expmnt = -1 - mask;
+
+int binLeng( unsigned int bin ) {
+    for (int i = sizeof(int) * 8-1; i > 0; i--) {
+        if ( ( bin & (1 << i) ) > 0) {
+            return(i+1);
+        }
+    };
+    return(0);
+}
+
+
+float log2a( int n ) {
+    if (n == 0) {return(0.0F);}
+    
+    int l = binLeng(n);
+    float r;
+    r = (n & ( 1 << ( l - 1 ) ) - 1 );
+    int tmp = (( (* (int *) &r) & expmnt) -((l-1)  << 23))  | (( (* (int *) &r) & mask));
+   
+
+    return( ((float) l - 1) + * (float *) &tmp );
+
+}
