eef1f52308c3d782bd1493d55559e30e16516441
sandem45 <116912414+SyntaxError0x2D@users.noreply.github.com>
2023-04-18 09:04:29 +0300

    unreal
    for a reason which is beyond my mere mortal comprehension I had i+1 in there, removed it and removed all l-1's.

--- log2a.c
+++ log2a.c
@@ -6,7 +6,7 @@ const int expnt = -1 - mask;
 int binLeng( unsigned int bin ) {
     for (int i = sizeof(int) * 8-1; i > 0; i--) {
         if ( ( bin & (1 << i) ) > 0) {
-            return(i+1);
+            return(i);
         }
     };
     return(0);
@@ -17,8 +17,8 @@ float log2a( int n ) {
     if (n == 0) {return(0.0F);}
     
     int l = binLeng(n);
-    float r = (n & ( 1 << ( l - 1 ) ) - 1 );
-    int tmp = (( (* (int *) &r) & expnt) -((l-1)  << 23))  | (( (* (int *) &r) & mask));
+    float r = (n & ( 1 << l  ) - 1 );
+    int tmp = (( (* (int *) &r) & expnt) -( l  << 23))  | (( (* (int *) &r) & mask));
    
 
     return( ((float) l - 1) + * (float *) &tmp );
